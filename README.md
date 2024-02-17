61243597dbea3529167f8a4849dde2f0451a2d59
The Economist <29724411+econcz@users.noreply.github.com>
2024-02-17 01:43:37 +0100

    Major update
    Added filename filtering, GAMS, and AMPL

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Expandable module of statistical data utilities
 
-
-Requirements:
-- Jupyter Notebook, R, Stata, Julia, and Octave (at least some of the mentioned)
-- installed stata-kernel, r2py, and oct2py from PyPi
-- julia in PATH
+## Requirements:  
+- **Jupyter Notebook**, **R**, **Stata**, **Julia**, **GAMS**, **AMPL**, and **Octave/Matlab** (at least some of
+the mentioned)
+- installed **stata-kernel**, **r2py**, and **oct2py** from PyPi
+- **julia**, **gams**, and **ampl** in PATH
 
 ## Functions:
-1. autorun(path_data='...', *args, **kwargs)
-Cross-platform .ipynb, .R, .do, .jl, and .m file batch runner.
+`autorun(path_data='...', pattern='...')`  
+
+Cross-platform .ipynb, .R, .do, .jl, .gms, .run, and .m file **batch runner**.
--- mydatau/mydatau.py
+++ mydatau/mydatau.py
@@ -1,34 +1,45 @@
 from os                  import chdir, getcwd, name, rename, system, walk
+from re                  import match, I
 from papermill           import execute_notebook
 from rpy2.robjects       import r
 from stata_kernel.config import config as stata_config
 from oct2py              import Oct2Py
 
 # autorun function
-def autorun(path_data=getcwd(), *args, **kwargs):
+def autorun(
+    path_data=getcwd(), pattern='', *args, **kwargs
+):
     stata  = stata_config.get('stata_path')
-    julia  = 'julia'                                   # must be in PATH
+    julia  = 'julia'                                   # must be in PATH        
+    gams   = 'gams'
+    ampl   = 'ampl'
     octave = Oct2Py()
 
-    chdir(path_data)                                   # cd to DATA/<folder>
+    chdir(path_data)                                   # cd to DATA/<folder>    
     for root, dirs, files in walk('.'):
         for file in files:
             # 1. Jupyter
-            if file.endswith('.ipynb'):
+            if match(pattern, file, I) and file.endswith('.ipynb'):
                 execute_notebook(file, file, kwargs)
             # 2. R
-            if file.endswith('.R'):
+            if match(pattern, file, I) and file.endswith('.R'):
                 with open(file + '.log', 'w') as f:
                     f.write(str(r.source(file)))
             # 3. Stata BE/SE/MP
-            if file.endswith('.do'):
+            if match(pattern, file, I) and file.endswith('.do'):
                 system(stata + (' /' if name == 'nt' else ' -') + 'bq ' + file)
                 rename(file.replace('.do', '.log'), file + '.log')
             # 4. Julia
-            if file.endswith('.jl'):
+            if match(pattern, file, I) and file.endswith('.jl'):
                 system(julia + ' ' + file + ' >' + file + '.log 2>&1')
-            # 5. Octave/Matlab
-            if file.endswith('.m'):
+            # 5. GAMS
+            if match(pattern, file, I) and file.endswith('.gms'):
+                system(gams + ' ' + file + ' >' + file + '.log 2>&1')
+            # 6. AMPL
+            if match(pattern, file, I) and file.endswith('.run'):
+                system(ampl + ' ' + file + ' >' + file + '.log 2>&1')
+            # 7. Octave/Matlab
+            if match(pattern, file, I) and file.endswith('.m'):
                 with open(file + '.log', 'w') as f:
                     f.write(str(octave.eval(file.replace('.m', '()'))))
         break
